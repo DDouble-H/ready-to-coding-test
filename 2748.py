fe750921521b9530eb244eafa0485f71241c19c2
ddouble-h <hyehyunwoo@gmail.com>
2020-04-20 17:10:22 +0900

    Create 2748.py

---
+++ 2748.py
@@ -0,0 +1,12 @@
+import sys
+N = int(sys.stdin.readline())
+
+def fibonacci(N):
+    cache = [0 for index in range(N+1)]
+    cache[0] = 0
+    cache[1] = 1
+    for index in range(2, N+1):
+        cache[index] = cache[index-1] + cache[index-2]
+    return cache[N]
+
+print(fibonacci(N))
